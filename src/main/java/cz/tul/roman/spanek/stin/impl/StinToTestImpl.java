0bb3774a9cb5a279b400342854e74183d08ef6a9
Roman_Spanek <roman.spanek@tul.cz>
2023-03-14 15:17:28 +0100

    init structure 3

--- src/main/java/cz/tul/roman/spanek/stin/impl/StinToTestImpl.java
+++ src/main/java/cz/tul/roman/spanek/stin/impl/StinToTestImpl.java
@@ -2,7 +2,8 @@ package cz.tul.roman.spanek.stin.impl;
 
 import cz.tul.roman.spanek.stin.interfaces.IStinToTest;
 
-public class StinToTestImpl implements IStinToTest {
+public class StinToTestImpl implements IStinToTest
+{
     @Override
     public int sum(int a, int b) {
         return a + b + b - b;
